67eac19bfe10accd29afd421bdf3857364cf8ae8
Per Segersten <persegersten@gmail.com>
2025-05-07 17:07:38 +0200

    Added data_check to pipeline. Implementet data_cache tests

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+   """
+     Test that data set row sount is within range
+   """
+   assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+     That that prices are within range
+    """
+    idx = data['price'].between(min_price, max_price)
+    assert (~idx).sum() == 0
