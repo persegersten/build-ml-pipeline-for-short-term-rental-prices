b6339570e6d3c7974150b66eb3cb7c07e786a910
Per Segersten <persegersten@gmail.com>
2025-05-28 21:37:27 +0200

    Updated defualt hyperparameters

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
-                    "mlflow_model": "random_forest_export:v16",
+                    "mlflow_model": "random_forest_export:prod",
                     "test_dataset": "test_data.csv:latest"
                 },
             )
